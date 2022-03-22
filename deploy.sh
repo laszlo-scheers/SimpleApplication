6f40c81e9435eafa1079a8c9f87dd4a24c3331c5
Laszlo-VSC <laszloluque@gmail.com>
2022-03-22 14:39:40 +0000

    security added

--- deploy.sh
+++ deploy.sh
@@ -4,11 +4,12 @@ sudo apt update && sudo apt install nodejs npm
 sudo npm install -g pm2
 # stop any instance of our application running currently
 pm2 stop example_app
+pm2 stop simple_app
 # change directory into folder where application is downloaded
-cd ExampleApplication/
+cd SimpleApplication/
 # Install application dependencies
 npm install
 echo $PRIVATE_KEY > privatekey.pem
 echo $SERVER > server.crt
 # Start the application with the process name example_app using pm2
-pm2 start ./bin/www --name example_app
+pm2 start ./bin/www --name simple_app
