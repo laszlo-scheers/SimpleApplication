e5728ebefa35b8e01950ec084e751b2bb9b4fe56
Laszlo-VSC <laszloluque@gmail.com>
2022-03-30 19:32:59 +0100

    added a command to stop the lab instance in a docker image

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,15 @@ sudo apt update && sudo apt install nodejs npm
 # Install pm2 which is a production process manager for Node.js with a built-in load balancer
 sudo npm install -g pm2
 # stop any instance of our application running currently
-pm2 stop example_app
 pm2 stop simple_app
+# Check if there is instance running with the image name we are deploying
+CURRENT_INSTANCE=$(docker ps -a -q --filter ancestor="laszloscheers/exampleapp" --format="{{.ID}}")
+
+#If an instance does exist, stops the instance
+if [ "$CURRENT_INSTANCE" ]
+then
+    docker rm $(docker stop $CURRENT_INSTANCE)
+fi
 # change directory into folder where application is downloaded
 cd SimpleApplication/
 # Install application dependencies
